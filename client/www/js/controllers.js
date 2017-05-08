0d79d89aab751c007f1be7091e6d9005aed46267
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-05-08 15:37:35 -0300

    add basic functionality

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -44,6 +44,18 @@ angular.module('starter.controllers', [])
 	};
 	setTimeout($scope.refreshPage, 1000);
 	ionicMaterialMotion.ripple();
+
+	$scope.shareAnywhere = function() {
+		$cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+}
+
+$scope.shareViaTwitter = function(message, image, link) {
+		$cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
+				$cordovaSocialSharing.shareViaTwitter(message, image, link);
+		}, function(error) {
+				alert("Cannot share on Twitter");
+		});
+}
 })
 
 .controller('AboutCtrl', function AboutCtrl($rootScope, $scope, ionicMaterialMotion, AboutFactory) {
